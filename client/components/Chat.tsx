21569f2b20c24856a987c3a5b7ed9e77632f104f
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-04 01:18:45 +0300

    added basic chat

--- client/components/Chat.tsx
+++ client/components/Chat.tsx
@@ -2,7 +2,38 @@ import React from "react";
 import styles from "../styles/chat.module.css";
 
 const Chat = () => {
-  return <div className={styles.container}>Chat</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.right_text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ullam.
+      </div>
+      <div className={styles.left_text}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit,
+        natus!
+      </div>
+      <div className={styles.right_text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ullam.
+      </div>
+      <div className={styles.left_text}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit,
+        natus!
+      </div>
+      <div className={styles.right_text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ullam.
+      </div>
+      <div className={styles.left_text}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit,
+        natus!
+      </div>
+      <div className={styles.right_text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ullam.
+      </div>
+      <div className={styles.left_text}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit,
+        natus!
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
