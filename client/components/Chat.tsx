0ba28d313ea33e059628df65d00f7453f379c65b
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-04 02:45:45 +0300

    added ticks

--- client/components/Chat.tsx
+++ client/components/Chat.tsx
@@ -5,6 +5,87 @@ const Chat = () => {
   return (
     <div className={styles.container}>
       <div className={styles.right_text}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
+          ullam.
+        </p>
+        <div className={styles.metadata}>
+          <p>2:11</p>
+          <img
+            src="128px-Blue_double_ticks.svg.png"
+            alt="tick"
+            className={styles.tick}
+          />
+        </div>
+      </div>
+      <div className={styles.left_text}>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Reprehenderit, natus!
+        </p>
+        <div className={styles.metadata}>
+          <p>2:11</p>
+          <img
+            src="check-mark-grey.svg.med.png"
+            alt="grey-tick"
+            className={styles.tick}
+          />
+        </div>
+      </div>
+      <div className={styles.right_text}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta,
+          ullam.
+        </p>
+        <div className={styles.metadata}>
+          <p>2:11</p>
+          <img
+            src="clipart1064340.png"
+            alt="single-tick"
+            className={styles.tick_rotate}
+          />
+        </div>
+      </div>
+      <div className={styles.left_text}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit,
+        natus!
+      </div>
+      <div className={styles.right_text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ullam.
+      </div>
+      <div className={styles.left_text}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit,
+        natus!
+      </div>
+      <div className={styles.right_text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ullam.
+      </div>
+      <div className={styles.left_text}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit,
+        natus!
+      </div>
+      <div className={styles.right_text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ullam.
+      </div>
+      <div className={styles.left_text}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit,
+        natus!
+      </div>
+      <div className={styles.right_text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ullam.
+      </div>
+      <div className={styles.left_text}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit,
+        natus!
+      </div>
+      <div className={styles.right_text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ullam.
+      </div>
+      <div className={styles.left_text}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit,
+        natus!
+      </div>
+      <div className={styles.right_text}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, ullam.
       </div>
       <div className={styles.left_text}>
