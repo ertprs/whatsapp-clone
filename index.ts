787d7a72faaae1b3ade53786a69cea9bdca7cce2
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-04 19:15:59 +0300

    added basic server setup

---
+++ index.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const connectMongo = async (): Promise<void> => {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("Mongo URI Must be provided");
+    }
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+    console.log("Connected to db");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connectMongo();
+
+app.listen(5000, () => console.log("Server started on port 5000"));
