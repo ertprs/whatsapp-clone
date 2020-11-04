276cd17d036d1de0c1fdceeb0346690ad0499cc3
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-04 12:03:21 +0300

    wired up contact header

--- client/components/Contacts.tsx
+++ client/components/Contacts.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import styles from "../styles/contacts.module.css";
+import { MdMessage } from "react-icons/md";
 
 const Main = () => {
   return (
     <div className={styles.container}>
       <div className={styles.profile}>
         <img className={styles.profile_img} src="portitem1.jpeg" alt="" />
-        <div className={styles.icon_box}>
-          <div className={styles.select_icon}></div>
-          <div className={styles.select_icon}></div>
-          <div className={styles.select_icon}></div>
+        <div className={styles.header_icons}>
+          <MdMessage size="30px" className={styles.MdMessage} />
+          <div className={styles.icon_box}>
+            <div className={styles.select_icon}></div>
+            <div className={styles.select_icon}></div>
+            <div className={styles.select_icon}></div>
+          </div>
         </div>
       </div>
+      <div className={styles.profile}></div>
       <div className={styles.profile}>
         <img className={styles.profile_img} src="portitem1.jpeg" alt="" />
         <div className={styles.user}>
