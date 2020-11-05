6f52928ac861963ee6e5d5123f3baef570550689
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-05 14:10:13 +0300

    added withoutauth hoc

--- client/HOCs/withoutAuth.tsx
+++ client/HOCs/withoutAuth.tsx
@@ -1,13 +1,13 @@
-import React, { useContext } from "react";
+import { NextPage, NextPageContext } from "next";
+import { useContext } from "react";
 import { UserContext } from "../Context/userContext";
 import Router from "next/router";
-import { NextPage, NextPageContext } from "next";
 
-export const withAuth = (WrappedComponent: NextPage): React.FC => {
+export const withoutAuth = (WrappedComponent: NextPage): React.FC => {
   const HocComponent = (props: any): JSX.Element => {
     const { user } = useContext(UserContext);
-    if (typeof window !== "undefined" && !user) {
-      Router.push("/login");
+    if (typeof window !== "undefined" && user) {
+      Router.back();
       return <></>;
     }
     return <WrappedComponent {...props} />;
@@ -15,15 +15,15 @@ export const withAuth = (WrappedComponent: NextPage): React.FC => {
 
   HocComponent.getInitialProps = async (ctx: NextPageContext) => {
     if (typeof window === "undefined" && !ctx.req?.headers.cookie && ctx.res) {
-      ctx.res.writeHead(301, { Location: "/login" });
+      ctx.res.writeHead(301, { Location: "/" });
       ctx.res.end();
     }
+
     let componentProps = {};
     if (WrappedComponent.getInitialProps) {
       componentProps = await WrappedComponent.getInitialProps(ctx);
     }
     return { ...componentProps };
   };
-
   return HocComponent;
 };
