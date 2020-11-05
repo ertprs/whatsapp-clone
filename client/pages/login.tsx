5d4289ce9121f76a950507c499587bbda9b3498a
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-05 23:41:56 +0300

    added rf to login

--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -2,25 +2,32 @@ import React from "react";
 import Link from "next/link";
 import { withoutAuth } from "../HOCs/withoutAuth";
 import styles from "../styles/login.module.css";
+import { Field, InjectedFormProps, reduxForm } from "redux-form";
+import Input from "../components/input";
 
-const login = () => {
+interface FormValues {
+  email: string;
+  password: string;
+}
+
+const login: React.FC<InjectedFormProps<FormValues>> = props => {
   return (
     <div className="container">
       <div className="parent_form login">
         <form className="form">
           <h1 className="login_h1">Login</h1>
-          <div className="form_group">
-            <input type="text" className="input" placeholder="Email" />{" "}
-            <label className="form_label" htmlFor="email">
-              Email
-            </label>
-          </div>
-          <div className="form_group">
-            <input type="text" className="input" placeholder="Password" />{" "}
-            <label className="form_label" htmlFor="password">
-              Password
-            </label>
-          </div>
+          <Field
+            component={Input}
+            placeholder="Email"
+            label="Email"
+            name="email"
+          />
+          <Field
+            component={Input}
+            placeholder="Password"
+            label="Password"
+            name="password"
+          />
           <button className="btn">Login</button>
           <p>
             Don't have an account?{" "}
@@ -34,4 +41,10 @@ const login = () => {
   );
 };
 
-export default withoutAuth(login);
+// const validate=(formValues)=>{
+
+// }
+
+export default withoutAuth(
+  reduxForm<FormValues>({ form: "login" })(login)
+);
