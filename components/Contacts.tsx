611f365c57c2030420a7afb14f34fbddbb89f977
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-06 15:46:42 +0300

    added menu toggle

--- components/Contacts.tsx
+++ components/Contacts.tsx
@@ -5,22 +5,29 @@ import { BiSearchAlt } from "react-icons/bi";
 
 const Main = () => {
   const [hideIcon, setHideIcon] = useState<boolean>(false);
+  const [hideMenu, setHideMenu] = useState<boolean>(false);
   return (
     <div className={styles.container}>
       <div className={`${styles.profile} ${styles.fixed}`}>
-        {/* <div className={styles.fixed}> */}
         <img className={styles.profile_img} src="portitem1.jpeg" alt="" />
         <div className={styles.header_icons}>
           <MdMessage size="30px" className={styles.MdMessage} />
-          <div className={styles.icon_box}>
+          <div
+            className={styles.icon_box}
+            onClick={() => setHideMenu(hide => !hide)}
+          >
             <div className={styles.select_icon}></div>
             <div className={styles.select_icon}></div>
             <div className={styles.select_icon}></div>
           </div>
-          {/* </div> */}
         </div>
       </div>
-      <div className={`${styles.profile}`}></div>
+      <div className={`${styles.profile}`}>
+        <div className={`${styles.box} ${hideMenu && styles.hideMenu}`}>
+          {" "}
+          box
+        </div>
+      </div>
       <div className={styles.profile}>
         <input
           type="text"
