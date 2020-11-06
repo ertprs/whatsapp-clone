d18f896f0e1d851acbd707821182386fbce87597
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-06 18:50:33 +0300

    added all messages endpoint

--- server/routes/messageRoutes.ts
+++ server/routes/messageRoutes.ts
@@ -9,15 +9,27 @@ const route = Router();
 route.post(
   "/new/message",
   auth,
-  check("from").trim().notEmpty().withMessage("from field cannot be empty"),
   check("to").trim().notEmpty().withMessage("to field cannot be empty"),
   check("message").trim().notEmpty().withMessage("message cannot be empty"),
   validateRequest,
   async (req: Request, res: Response): Promise<void> => {
-    const { from, to, message } = req.body;
-    const newMessage = Message.build({ from, to, message });
+    const { to, message } = req.body;
+    const newMessage = Message.build({
+      from: req.session?.user._id,
+      to,
+      message
+    });
     await newMessage.save();
     res.send(newMessage);
+  }
+);
+
+route.get(
+  "/all/messages",
+  auth,
+  async (req: Request, res: Response): Promise<void> => {
+    const messages = await Message.find({ from: req.session?.user._id });
+    res.send(messages);
   }
 );
 
