54eee717f54e24ab7183df17b2c287a000691bf5
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-08 12:48:53 +0300

    added last msg schema

---
+++ server/models/LastMsg.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+interface LastMsgAttrs {
+  from: mongoose.Types.ObjectId;
+  to: mongoose.Types.ObjectId;
+  message: string;
+}
+
+interface LastMsgDoc extends mongoose.Document {
+  from: mongoose.Types.ObjectId;
+  to: mongoose.Types.ObjectId;
+  message: string;
+}
+
+interface LastMsgModel extends mongoose.Model<LastMsgDoc> {
+  build(attrs: LastMsgAttrs): LastMsgDoc;
+}
+
+const LastMsgSchema = new mongoose.Schema(
+  {
+    from: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "User"
+    },
+    to: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "User"
+    },
+    message: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+LastMsgSchema.statics.build = (attrs: LastMsgAttrs): LastMsgDoc => {
+  return new LastMsg(attrs);
+};
+
+const LastMsg = mongoose.model<LastMsgDoc, LastMsgModel>(
+  "LastMsg",
+  LastMsgSchema
+);
+
+export { LastMsg };
