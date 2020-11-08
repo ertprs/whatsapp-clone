2cd469b7e66fbaa33931d07c5db53aa3c0f0cbf3
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-08 12:30:40 +0300

    added fetching messages spinner

--- components/WithoutChat.tsx
+++ components/WithoutChat.tsx
@@ -3,8 +3,10 @@ import styles from "../styles/withoutChat.module.css";
 
 const WithoutChat = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.img}>
+    <div className={`${styles.container} ${styles.spinner}`}>
+      <div className={`ui active centered inline loader `}></div>
+      <p>fetching messages</p>
+      {/* <div className={styles.img}>
         <img
           src="/intro-connection-light_c98cc75f2aa905314d74375a975d2cf2.jpg"
           alt=""
@@ -16,7 +18,7 @@ const WithoutChat = () => {
           WhatsApp connects to your phone to sync messages. To reduce data
           usage, connect your phone to Wi-Fi.
         </p>
-      </div>
+      </div> */}
     </div>
   );
 };
