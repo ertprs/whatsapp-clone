9549b22f7e8cfe6c3cc7a41291cecd3a6bd4120f
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-14 20:16:46 +0300

    heroku

--- Axios.ts
+++ Axios.ts
@@ -3,8 +3,10 @@ import Axios from "axios";
 export const axios =
   process.env.NODE_ENV === "development"
     ? Axios.create({
-        baseURL: "http://localhost:3000"
+        baseURL: "http://localhost:3000",
+        withCredentials: true
       })
     : Axios.create({
-        baseURL: "https://whatsapp-2.herokuapp.com/"
+        baseURL: "https://whatsapp-2.herokuapp.com/",
+        withCredentials: true
       });
