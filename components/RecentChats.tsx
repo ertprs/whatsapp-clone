f1ba0d73df944f09329ebe3f261298400c685659
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-15 20:26:52 +0300

    added recent chat component

---
+++ components/RecentChats.tsx
@@ -0,0 +1,77 @@
+import styles from "../styles/contacts.module.css";
+import React from "react";
+import { formatDistance } from "date-fns";
+import { Message } from "../interfaces/Message";
+import { User } from "../interfaces/User";
+import {
+  AddCurrentContact,
+  addCurrentContact,
+  fetchMessages,
+  SetDisplay,
+  setDisplay
+} from "../redux/actions";
+import { connect } from "react-redux";
+
+interface Props {
+  filteredRecentChats: Message[] | [];
+  currentUser: User | null;
+  addCurrentContact: (user: User) => AddCurrentContact;
+  fetchMessages: Function;
+  setDisplay: (display: boolean) => SetDisplay;
+  messages: Message[] | [];
+}
+
+const RecentChats: React.FC<Props> = props => {
+  return (
+    <React.Fragment>
+      {props.filteredRecentChats &&
+        props.filteredRecentChats.length !== 0 &&
+        (props.filteredRecentChats as Message[]).map(msg => (
+          <div
+            className={styles.profile}
+            key={msg._id}
+            onClick={() => {
+              if (props.currentUser?._id.toString() === msg.to._id.toString()) {
+                props.addCurrentContact(msg.from);
+                props.fetchMessages(msg.from._id);
+              } else {
+                props.addCurrentContact(msg.to);
+                props.fetchMessages(msg.to._id);
+              }
+              props.setDisplay(false);
+            }}
+          >
+            <img className={styles.profile_img} src="portitem1.jpeg" alt="" />
+            <div className={styles.user}>
+              <div className={styles.user_header}>
+                <h2>
+                  {props.currentUser?._id.toString() === msg.to._id.toString()
+                    ? `${msg.from.firstName} ${msg.from.lastName}`
+                    : `${msg.to.firstName} ${msg.to.lastName}`}
+                </h2>
+                <p>
+                  {props.messages && props.messages.length !== 0
+                    ? formatDistance(
+                        new Date(
+                          props.messages![props.messages!.length - 1]
+                            .createdAt as string
+                        ),
+                        Date.now()
+                      )
+                    : formatDistance(
+                        new Date(msg.updatedAt as string),
+                        Date.now()
+                      )}
+                </p>
+              </div>
+              <p>{msg.message}</p>
+            </div>
+          </div>
+        ))}
+    </React.Fragment>
+  );
+};
+
+export default connect(null, { addCurrentContact, fetchMessages, setDisplay })(
+  RecentChats
+);
