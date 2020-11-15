1cf4fd80201694e63e489ce91f6a4d2d7bb894c1
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-15 20:13:47 +0300

    added header component

---
+++ components/Header.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { BiSearchAlt } from "react-icons/bi";
+import { MdMessage } from "react-icons/md";
+import { connect } from "react-redux";
+import { FilterRecentChats, filterRecentChats } from "../redux/actions";
+import styles from "../styles/contacts.module.css";
+
+interface Props {
+  setNewChat: React.Dispatch<React.SetStateAction<boolean>>;
+  setHideMenu: React.Dispatch<React.SetStateAction<boolean>>;
+  hideMenu: boolean;
+  filterRecentChats: (text: string) => FilterRecentChats;
+  setHideIcon: React.Dispatch<React.SetStateAction<boolean>>;
+  hideIcon: boolean;
+}
+
+const Header: React.FC<Props> = props => {
+  return (
+    <div className={`${styles.profile} ${styles.fixed_2} ${styles.header}`}>
+      <img className={styles.profile_header_img} src="portitem1.jpeg" alt="" />
+      <div className={styles.header_icons}>
+        <MdMessage
+          size="30px"
+          className={styles.MdMessage}
+          onClick={() => props.setNewChat(chat => !chat)}
+        />
+        <div
+          className={`${styles.icon_box} ${
+            !props.hideMenu && styles.icon_box_color
+          }`}
+          onClick={() => props.setHideMenu(hide => !hide)}
+        >
+          <div className={styles.select_icon}></div>
+          <div className={styles.select_icon}></div>
+          <div className={styles.select_icon}></div>
+        </div>
+      </div>
+      <div
+        className={`${styles.profile_input} ${styles.search} ${styles.fixed_input}`}
+      >
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Search or start a new chat"
+          onChange={e => {
+            props.filterRecentChats(e.target.value);
+            props.setHideIcon(true);
+          }}
+          onMouseLeave={() => props.setHideIcon(false)}
+        />
+        <BiSearchAlt
+          className={`${styles.BiSearchAlt} ${
+            props.hideIcon && styles.hide_icon
+          }`}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default connect(null, { filterRecentChats })(Header);
