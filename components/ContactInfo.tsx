df5afdd6ee2e05343f0314ef6276123b452138e0
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-16 14:11:19 +0300

    added custom scroll bar to chat container

--- components/ContactInfo.tsx
+++ components/ContactInfo.tsx
@@ -1,17 +1,9 @@
 import React from "react";
+import styles from "../styles/contactinfo.module.css";
 
 const ContactInfo = () => {
   return (
-    <div
-      style={{
-        height: "100vh",
-        width: "27vw",
-        backgroundColor: "black",
-        position: "fixed",
-        top: "0",
-        right: "0"
-      }}
-    >
+    <div className={styles.container}>
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis
       voluptatum facere iure sapiente ab! Fugit illum cupiditate id magnam
       tenetur, eaque aut molestiae! Odit officia, tempore, laudantium voluptates
