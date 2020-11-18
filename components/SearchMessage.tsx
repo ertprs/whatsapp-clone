16e5cfffc794b86aabbfa888ecb0d8d8fb3f6aa8
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-18 13:49:27 +0300

    styled the tex input

--- components/SearchMessage.tsx
+++ components/SearchMessage.tsx
@@ -1,33 +1,22 @@
 import React from "react";
+import { AiOutlineArrowLeft, AiOutlineSearch } from "react-icons/ai";
+import styles from "../styles/searchMessage.module.css";
 
 const SearchMessage: React.FC = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis soluta
-      repellendus omnis maxime dolorem quisquam saepe labore debitis nam
-      repellat aspernatur voluptate repudiandae magni, blanditiis ea autem harum
-      totam dicta. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Autem quibusdam quidem consequatur aut vel veritatis tenetur et tempora
-      sapiente obcaecati labore recusandae architecto non quod officiis, alias
-      adipisci nostrum quisquam nihil atque? Nihil ad consequuntur modi voluptas
-      suscipit velit eius sequi quis deserunt eos reprehenderit in accusantium
-      voluptatibus laudantium tenetur quisquam, quos distinctio cupiditate
-      commodi inventore. Laboriosam, nesciunt! Quia odit aliquid repellendus
-      repellat voluptate, ipsam eligendi neque voluptatibus, blanditiis ducimus
-      nihil amet suscipit eaque modi fuga, recusandae quod dolorum! Numquam sed
-      ipsa voluptates amet corrupti alias, dolor a voluptate repudiandae porro
-      pariatur cupiditate, quas quidem velit, voluptas officiis. Aliquid harum
-      blanditiis, quas nam odio sapiente placeat impedit dicta quam natus saepe.
-      Laudantium exercitationem ab ex perferendis quos sit. Quaerat inventore
-      obcaecati rerum itaque exercitationem quo maxime! Neque similique
-      recusandae labore ipsam qui ipsa voluptatum sequi, pariatur totam fugiat
-      expedita accusamus a autem nostrum voluptas? Autem expedita a et
-      laboriosam voluptates! Eaque soluta voluptate aliquid. Quisquam suscipit,
-      incidunt consectetur adipisci odit cum itaque amet iste commodi expedita
-      doloremque maiores perferendis dignissimos? Delectus, quo. Ex
-      necessitatibus delectus quod beatae modi dolorem nesciunt, doloremque
-      provident cumque. Sed provident pariatur ducimus et? Molestias ea officia
-      error quibusdam quam sequi dolores recusandae esse id commodi?
+    <div className={styles.container}>
+      <div className={styles.search}>
+        <div>
+          <div>
+            <AiOutlineSearch size="20px" />
+          </div>
+          <div className={styles.AiOutlineArrowLeft}>
+            <AiOutlineArrowLeft size="20px" />
+          </div>
+        </div>
+        <input type="text" placeholder="Search..." />
+      </div>
+      <div className={styles.messages}>messages</div>
     </div>
   );
 };
