63706183621171d7ad951a13dc95be14d1cea5d1
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-18 14:14:46 +0300

    wired up search messages

--- components/SearchMessage.tsx
+++ components/SearchMessage.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineArrowLeft, AiOutlineSearch } from "react-icons/ai";
 import styles from "../styles/searchMessage.module.css";
 
 const SearchMessage: React.FC = () => {
+  const [focused, setFocused] = useState<boolean>(false);
   return (
     <div className={styles.container}>
       <div className={styles.search}>
         <div>
-          <div>
-            <AiOutlineSearch size="20px" />
+          <div
+            className={
+              focused ? styles.AiOutlineSearch__hidden : styles.AiOutlineSearch
+            }
+          >
+            <AiOutlineSearch size="20px" color="rgb(80,80,80)" />
           </div>
-          <div className={styles.AiOutlineArrowLeft}>
-            <AiOutlineArrowLeft size="20px" />
+          <div
+            className={
+              focused ? styles.AiOutlineArrowLeft : styles.AiOutlineArrowBottom
+            }
+          >
+            <AiOutlineArrowLeft size="20px" color="#009688" />
           </div>
         </div>
-        <input type="text" placeholder="Search..." />
+        <input
+          type="text"
+          placeholder="Search..."
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+        />
       </div>
       <div className={styles.messages}>messages</div>
     </div>
