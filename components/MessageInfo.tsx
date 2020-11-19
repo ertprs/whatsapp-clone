f1cf75397219eddd6de985f8d81b649f1325518d
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-19 23:13:57 +0300

    added scrolling to the message

--- components/MessageInfo.tsx
+++ components/MessageInfo.tsx
@@ -9,7 +9,29 @@ const MessageInfo = () => {
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus
         porro eveniet nobis fugiat quasi neque sequi consequuntur, voluptas id
         dignissimos, deleniti accusamus magnam autem repellendus quos eius sed
-        nemo cum?
+        nemo cum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+        corrupti quis dolore nulla fugiat adipisci, libero odio minus molestiae
+        esse deserunt provident impedit ex possimus facere, doloribus eos atque
+        voluptatem nam laboriosam explicabo sequi commodi! Earum voluptas, quo
+        delectus accusantium, a ab soluta numquam adipisci repellat magnam
+        possimus quia eius voluptatum laboriosam hic eveniet temporibus
+        exercitationem harum quos consequuntur vitae nulla voluptatibus veniam
+        libero. Error officia eligendi odit repellendus in similique doloribus
+        optio, distinctio tenetur minus enim porro, unde vitae maxime
+        consectetur eveniet autem laboriosam dolore obcaecati laborum nam
+        voluptate. Possimus unde nostrum, suscipit provident, debitis
+        consectetur, a ullam consequatur quam dolor eligendi blanditiis?
+        Quisquam harum eligendi facere accusantium facilis dolores corrupti
+        ullam nam magni tempore esse nulla labore maiores quae hic sapiente quis
+        placeat sequi expedita, similique exercitationem provident est? Earum
+        quasi eligendi dolores amet modi voluptatibus rerum veritatis autem est
+        totam, omnis distinctio accusamus dolorem adipisci ullam atque laborum
+        rem incidunt. Amet officia vero, iure dolorem quaerat nostrum aliquam
+        veniam impedit. Ex totam vitae necessitatibus. Maxime, illo quis! Eius
+        repudiandae corporis a dignissimos quaerat quae aspernatur dolorum neque
+        autem consequuntur possimus totam voluptas necessitatibus fuga,
+        voluptatum odio, eveniet consequatur facere. Porro similique accusamus
+        earum atque odit distinctio vel.
       </div>
       <div className={styles.message_info}>
         <div>
@@ -23,7 +45,7 @@ const MessageInfo = () => {
             </span>
             <span>Read</span>
           </p>
-          <p>7:30pm</p>
+          <p className={styles.time}>Yesterday at 7:30pm</p>
         </div>
         <div>
           <p>
@@ -36,7 +58,7 @@ const MessageInfo = () => {
             </span>
             <span>Delivered</span>
           </p>
-          <p>7:30pm</p>
+          <p className={styles.time}>Yesterday at 7:30pm</p>
         </div>
       </div>
     </div>
