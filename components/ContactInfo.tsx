5677475c11ba04e7ab3523db1edce9c1aecb24cb
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-20 13:03:45 +0300

    populated contact info page

--- components/ContactInfo.tsx
+++ components/ContactInfo.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import { IoIosArrowForward, IoMdThumbsDown } from "react-icons/io";
 import { toggleContactInfo, ToggleContactInfo } from "../redux/actions";
 import styles from "../styles/contactinfo.module.css";
 import { MdDelete } from "react-icons/md";
 import { BiBlock } from "react-icons/bi";
+import { Redux } from "../interfaces/Redux";
+import { formatRelative } from "date-fns";
 
 interface Props {
   toggleContactInfo: (toggle: boolean) => ToggleContactInfo;
 }
 
 const ContactInfo: React.FC<Props> = props => {
+  const currentContact = useSelector(
+    (state: Redux) => state.user.currentContact
+  );
   return (
     <div className={styles.container}>
       <div className={styles.contactinfo_header}>
@@ -30,8 +35,17 @@ const ContactInfo: React.FC<Props> = props => {
           className={styles.profile_img}
         />
         <div>
-          <h1>Kevin</h1>
-          <p>Last Seen Friday at 10:13pm</p>
+          <h1>
+            {currentContact?.firstName} {currentContact?.lastName}
+          </h1>
+          <p>
+            {currentContact?.lastSeen
+              ? `Last seen ${formatRelative(
+                  new Date(currentContact.lastSeen),
+                  Date.now()
+                )}`
+              : ""}
+          </p>
         </div>
       </div>
       <div className={`${styles.action} ${styles.mld}`}>
@@ -57,9 +71,10 @@ const ContactInfo: React.FC<Props> = props => {
         </span>
       </div>
       <div className={`${styles.noti} ${styles.action}`}>
-        <p>About and Phone Number</p>
+        <p className={styles.a_e}>About and Email</p>
+        <span className={styles.status}>{currentContact?.status}</span>
         <span className={styles.border}>&nbsp;</span>
-        <p>+254712345678</p>
+        <p className={styles.email}>{currentContact?.email}</p>
       </div>
       <div className={styles.danger}>
         <div>
