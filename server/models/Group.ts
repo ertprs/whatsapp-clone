43160a2bbe8ea87984b85cee58e07e4f4b7c74f0
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-20 21:03:42 +0300

    added a group model

---
+++ server/models/Group.ts
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+
+interface GroupAttrs {
+  name: string;
+  from: mongoose.Types.ObjectId;
+  message: string;
+  participants: mongoose.Types.ObjectId[];
+  admin: mongoose.Types.ObjectId;
+}
+
+interface GroupDoc extends mongoose.Document {
+  name: string;
+  from: mongoose.Types.ObjectId;
+  message: string;
+  participants: mongoose.Types.ObjectId[];
+  admin: mongoose.Types.ObjectId;
+}
+
+interface GroupModel extends mongoose.Model<GroupDoc> {
+  build: (attrs: GroupAttrs) => GroupDoc;
+}
+
+const GroupSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    from: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    message: {
+      type: String,
+      required: true
+    },
+    participants: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true
+      }
+    ],
+    admin: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+GroupSchema.statics.build = (groupAttrs: GroupAttrs): GroupDoc => {
+  return new Group(groupAttrs);
+};
+
+const Group = mongoose.model<GroupDoc, GroupModel>("Group", GroupSchema);
+
+export { Group };
