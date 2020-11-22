6dd7ae910962f8d821097d452b8a7a0c6eab2c32
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-22 11:35:44 +0300

    styled contacts

--- components/Group/NewGroupContacts.tsx
+++ components/Group/NewGroupContacts.tsx
@@ -1,8 +1,41 @@
 import React from "react";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { useSelector } from "react-redux";
+import { Redux } from "../../interfaces/Redux";
+import { User } from "../../interfaces/User";
 import styles from "../../styles/NewGroupContacts.module.css";
 
 const NewGroupContacts = () => {
-  return <div className={styles.container}>NewGroupContacts</div>;
+  const contacts = useSelector((state: Redux) => state.user.contacts);
+  return (
+    <div className={styles.container}>
+      <div>
+        <div>
+          <AiOutlineArrowLeft />
+        </div>
+        <p>Add Group Participants</p>
+      </div>
+      <div>
+        <input type="text" placeholder="Type contact name" />
+      </div>
+      <div>
+        {contacts &&
+          contacts.length !== 0 &&
+          (contacts as User[]).map(user => (
+            <div className={styles.contacts}>
+              <img className={styles.profile_img} src="portitem1.jpeg" alt="" />
+              <div className={styles.user}>
+                <div className={styles.user_header}>
+                  <h2>
+                    {user.firstName} {user.lastName}
+                  </h2>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default NewGroupContacts;
