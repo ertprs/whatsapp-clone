0fd22c27f8341a4a1cd2ce5ae1fac58e247bff6c
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-23 20:00:42 +0300

    wired up input styling

--- components/Group/GroupSubject.tsx
+++ components/Group/GroupSubject.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import styles from "../../styles/groupSubject.module.css";
 
 const GroupSubject = () => {
+  const [input, setInput] = useState<string>("");
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -17,8 +18,12 @@ const GroupSubject = () => {
         <img src="blank-profile-picture-973460_640.png" alt="grp_img" />
       </div>
       <div className={styles.input}>
-        <input type="text" />
-        <div>
+        <input
+          type="text"
+          onChange={e => setInput(e.target.value)}
+          value={input}
+        />
+        <div className={input.length !== 0 ? styles.transform : ""}>
           <p>Group Subject</p>
         </div>
         <span>&nbsp;</span>
