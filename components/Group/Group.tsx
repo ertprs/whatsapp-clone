cc5a330dd7fa63b9ce9576f41653b6086feb1fca
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-24 02:27:30 +0300

    styled the group

--- components/Group/Group.tsx
+++ components/Group/Group.tsx
@@ -1,21 +1,47 @@
 import React from "react";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 import styles from "../../styles/group.module.css";
 
 const Group = () => {
   return (
     <div className={styles.container}>
-      <div>
-        <p>Header</p>
+      <div className={styles.header}>
+        <div>
+          <AiOutlineArrowLeft size="20px" />
+          <p>Header</p>
+        </div>
         <input type="text" />
       </div>
-      <div>
-        <div>
+      <div className={styles.body}>
+        <div className={styles.group}>
           <img src="portitem1.jpeg" alt="pfp" />
-          <div>
-            <p>Kevin</p>
-            <p>3 days ago</p>
+          <div className={styles.text_body}>
+            <div className={styles.metadata}>
+              <h2>Kevin</h2>
+              <p>3 days ago</p>
+            </div>
+            <div>Lorem ipsum dolor sit</div>
           </div>
-          <div>Lorem ipsum dolor sit</div>
+        </div>
+        <div className={styles.group}>
+          <img src="portitem1.jpeg" alt="pfp" />
+          <div className={styles.text_body}>
+            <div className={styles.metadata}>
+              <h2>Kevin</h2>
+              <p>3 days ago</p>
+            </div>
+            <div>Lorem ipsum dolor sit</div>
+          </div>
+        </div>
+        <div className={styles.group}>
+          <img src="portitem1.jpeg" alt="pfp" />
+          <div className={styles.text_body}>
+            <div className={styles.metadata}>
+              <h2>Kevin</h2>
+              <p>3 days ago</p>
+            </div>
+            <div>Lorem ipsum dolor sit</div>
+          </div>
         </div>
       </div>
     </div>
