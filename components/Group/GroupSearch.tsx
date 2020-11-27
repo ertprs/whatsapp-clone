c8d40e04101ee8bc5c91c5e504283927d7c4e45d
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-28 00:51:30 +0300

    added basic groupsearch layout

--- components/Group/GroupSearch.tsx
+++ components/Group/GroupSearch.tsx
@@ -1,10 +1,107 @@
 import React from "react";
+import { BsCheck } from "react-icons/bs";
 import styles from "../../styles/groupSearch.module.css";
 
 const GroupSearch = () => {
   return (
     <div>
-      <div className={styles.container}>GroupSearch</div>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <p>X</p>
+          <p>Search Messages</p>
+          <input type="text" />
+        </div>
+        <div className={styles.body}>
+          <div className={styles.message}>
+            <p>9 Minutes</p>
+            <div>
+              <BsCheck
+                size="17px"
+                style={{ transform: "rotate(-10deg)" }}
+                color="rgba(0,0,0,.5)"
+              />
+              <p>wassup</p>
+            </div>
+          </div>
+          <div className={styles.message}>
+            <p>9 Minutes</p>
+            <div>
+              <BsCheck
+                size="17px"
+                style={{ transform: "rotate(-10deg)" }}
+                color="rgba(0,0,0,.5)"
+              />
+              <p>wassup</p>
+            </div>
+          </div>
+          <div className={styles.message}>
+            <p>9 Minutes</p>
+            <div>
+              <BsCheck
+                size="17px"
+                style={{ transform: "rotate(-10deg)" }}
+                color="rgba(0,0,0,.5)"
+              />
+              <p>wassup</p>
+            </div>
+          </div>
+          <div className={styles.message}>
+            <p>9 Minutes</p>
+            <div>
+              <BsCheck
+                size="17px"
+                style={{ transform: "rotate(-10deg)" }}
+                color="rgba(0,0,0,.5)"
+              />
+              <p>wassup</p>
+            </div>
+          </div>
+          <div className={styles.message}>
+            <p>9 Minutes</p>
+            <div>
+              <BsCheck
+                size="17px"
+                style={{ transform: "rotate(-10deg)" }}
+                color="rgba(0,0,0,.5)"
+              />
+              <p>wassup</p>
+            </div>
+          </div>
+          <div className={styles.message}>
+            <p>9 Minutes</p>
+            <div>
+              <BsCheck
+                size="17px"
+                style={{ transform: "rotate(-10deg)" }}
+                color="rgba(0,0,0,.5)"
+              />
+              <p>wassup</p>
+            </div>
+          </div>
+          <div className={styles.message}>
+            <p>9 Minutes</p>
+            <div>
+              <BsCheck
+                size="17px"
+                style={{ transform: "rotate(-10deg)" }}
+                color="rgba(0,0,0,.5)"
+              />
+              <p>wassup</p>
+            </div>
+          </div>
+          <div className={styles.message}>
+            <p>9 Minutes</p>
+            <div>
+              <BsCheck
+                size="17px"
+                style={{ transform: "rotate(-10deg)" }}
+                color="rgba(0,0,0,.5)"
+              />
+              <p>wassup</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
