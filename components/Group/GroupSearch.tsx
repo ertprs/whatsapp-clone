b5f1bde24fe706e9fe470be0e19a0af9f8359023
KelvinMitaki <mitakikelvin1@gmail.com>
2020-11-28 20:04:59 +0300

    added ellipsis

--- components/Group/GroupSearch.tsx
+++ components/Group/GroupSearch.tsx
@@ -74,12 +74,40 @@ const GroupSearch: React.FC<Props> = props => {
           <div className={styles.message}>
             <p className={styles.date}>9 Minutes</p>
             <div>
-              <BsCheck
+              {/* <BsCheck
                 size="17px"
                 style={{ transform: "rotate(-10deg)" }}
                 color="rgba(0,0,0,.5)"
-              />
-              <p>wassup</p>
+              /> */}
+              <div className={styles.name}>Kevin:</div>
+              <p className={styles.text}>
+                wassup Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Maxime perferendis ex dolor ipsam. Natus fugiat incidunt
+                sapiente reprehenderit explicabo ratione quisquam facere libero
+                officiis labore sint excepturi ad aut, neque ducimus pariatur
+                ipsam veniam repellendus saepe? Perspiciatis aperiam consectetur
+                facilis nesciunt eos, ex consequuntur, quos laudantium iure
+                excepturi repellat repudiandae culpa deserunt id tempore
+                exercitationem voluptate facere veritatis reiciendis vero nulla
+                quisquam. Laudantium quaerat ipsum dolore, dolor quam vero
+                eveniet nesciunt ad perferendis reprehenderit voluptatibus,
+                dolorem excepturi provident enim maxime aspernatur ullam
+                dignissimos quos, ratione doloremque possimus est repellat iste
+                explicabo. A provident libero eum eveniet sed deserunt inventore
+                sunt tempore, itaque corrupti similique accusantium. Aliquid
+                earum nesciunt quas eaque, assumenda dolores exercitationem
+                nobis iste harum dolor vel veniam placeat, laboriosam rem eius
+                numquam. Non, nam quia eaque alias dignissimos at ea, sequi
+                harum vel porro eius ducimus odio libero dolore, excepturi illum
+                sed. Expedita sit magni excepturi nisi perspiciatis? Sit
+                voluptatibus laudantium distinctio mollitia possimus vitae in
+                impedit atque. Fuga quaerat porro, architecto atque nobis
+                tenetur ducimus cupiditate veniam itaque expedita optio
+                obcaecati esse, quos modi aperiam quis! Debitis repellendus fuga
+                laudantium deleniti asperiores. Doloremque similique quas
+                dignissimos veritatis, illum officia pariatur exercitationem
+                magni facere ad. Molestias, vitae iste?
+              </p>
             </div>
           </div>
           <div className={styles.message}>
