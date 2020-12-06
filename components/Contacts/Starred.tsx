8919f75e2bb59e7c823006582bd1015c8aaa739a
KelvinMitaki <mitakikelvin1@gmail.com>
2020-12-06 22:43:51 +0300

    wired up starred msgs styling

--- components/Contacts/Starred.tsx
+++ components/Contacts/Starred.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "../../styles/starred.module.css";
 import { FiArrowLeft } from "react-icons/fi";
+import { AiFillStar } from "react-icons/ai";
 
 const Starred = () => {
   return (
@@ -20,118 +21,199 @@ const Starred = () => {
         <div className={styles.message}>
           <div className={styles.msg_header}>
             <img className={styles.profile_img} src="portitem1.jpeg" alt="" />
-            <p>Kevin Mitaki</p>
-            <p>Test 15</p>
+            <div className={styles.from_to}>
+              <p>Kevin Mitaki</p>
+              <p>Test 15</p>
+            </div>
+            <p className={styles.time}>9:54 AM</p>
           </div>
           <div className={styles.msg_body}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-            odio dolor in, nesciunt officia dolores quaerat! Earum voluptatem
-            fugiat ut aliquid fugit, sint blanditiis quibusdam accusantium minus
-            et pariatur dignissimos?
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Similique odio dolor in, nesciunt officia dolores quaerat! Earum
+              voluptatem fugiat ut aliquid fugit, sint blanditiis quibusdam
+              accusantium minus et pariatur dignissimos?
+            </p>
+            <div className={styles.msg_time}>
+              <AiFillStar />
+              <p>10:30PM</p>
+            </div>
           </div>
         </div>
         <div className={styles.message}>
           <div className={styles.msg_header}>
             <img className={styles.profile_img} src="portitem1.jpeg" alt="" />
-            <p>Kevin Mitaki</p>
-            <p>Test 15</p>
+            <div className={styles.from_to}>
+              <p>Kevin Mitaki</p>
+              <p>Test 15</p>
+            </div>
+            <p className={styles.time}>9:54 AM</p>
           </div>
           <div className={styles.msg_body}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-            odio dolor in, nesciunt officia dolores quaerat! Earum voluptatem
-            fugiat ut aliquid fugit, sint blanditiis quibusdam accusantium minus
-            et pariatur dignissimos?
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Similique odio dolor in, nesciunt officia dolores quaerat! Earum
+              voluptatem fugiat ut aliquid fugit, sint blanditiis quibusdam
+              accusantium minus et pariatur dignissimos?
+            </p>
+            <div className={styles.msg_time}>
+              <AiFillStar />
+              <p>10:30PM</p>
+            </div>
           </div>
         </div>
         <div className={styles.message}>
           <div className={styles.msg_header}>
             <img className={styles.profile_img} src="portitem1.jpeg" alt="" />
-            <p>Kevin Mitaki</p>
-            <p>Test 15</p>
+            <div className={styles.from_to}>
+              <p>Kevin Mitaki</p>
+              <p>Test 15</p>
+            </div>
+            <p className={styles.time}>9:54 AM</p>
           </div>
           <div className={styles.msg_body}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-            odio dolor in, nesciunt officia dolores quaerat! Earum voluptatem
-            fugiat ut aliquid fugit, sint blanditiis quibusdam accusantium minus
-            et pariatur dignissimos?
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Similique odio dolor in, nesciunt officia dolores quaerat! Earum
+              voluptatem fugiat ut aliquid fugit, sint blanditiis quibusdam
+              accusantium minus et pariatur dignissimos?
+            </p>
+            <div className={styles.msg_time}>
+              <AiFillStar />
+              <p>10:30PM</p>
+            </div>
           </div>
         </div>
         <div className={styles.message}>
           <div className={styles.msg_header}>
             <img className={styles.profile_img} src="portitem1.jpeg" alt="" />
-            <p>Kevin Mitaki</p>
-            <p>Test 15</p>
+            <div className={styles.from_to}>
+              <p>Kevin Mitaki</p>
+              <p>Test 15</p>
+            </div>
+            <p className={styles.time}>9:54 AM</p>
           </div>
           <div className={styles.msg_body}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-            odio dolor in, nesciunt officia dolores quaerat! Earum voluptatem
-            fugiat ut aliquid fugit, sint blanditiis quibusdam accusantium minus
-            et pariatur dignissimos?
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Similique odio dolor in, nesciunt officia dolores quaerat! Earum
+              voluptatem fugiat ut aliquid fugit, sint blanditiis quibusdam
+              accusantium minus et pariatur dignissimos?
+            </p>
+            <div className={styles.msg_time}>
+              <AiFillStar />
+              <p>10:30PM</p>
+            </div>
           </div>
         </div>
         <div className={styles.message}>
           <div className={styles.msg_header}>
             <img className={styles.profile_img} src="portitem1.jpeg" alt="" />
-            <p>Kevin Mitaki</p>
-            <p>Test 15</p>
+            <div className={styles.from_to}>
+              <p>Kevin Mitaki</p>
+              <p>Test 15</p>
+            </div>
+            <p className={styles.time}>9:54 AM</p>
           </div>
           <div className={styles.msg_body}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-            odio dolor in, nesciunt officia dolores quaerat! Earum voluptatem
-            fugiat ut aliquid fugit, sint blanditiis quibusdam accusantium minus
-            et pariatur dignissimos?
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Similique odio dolor in, nesciunt officia dolores quaerat! Earum
+              voluptatem fugiat ut aliquid fugit, sint blanditiis quibusdam
+              accusantium minus et pariatur dignissimos?
+            </p>
+            <div className={styles.msg_time}>
+              <AiFillStar />
+              <p>10:30PM</p>
+            </div>
           </div>
         </div>
         <div className={styles.message}>
           <div className={styles.msg_header}>
             <img className={styles.profile_img} src="portitem1.jpeg" alt="" />
-            <p>Kevin Mitaki</p>
-            <p>Test 15</p>
+            <div className={styles.from_to}>
+              <p>Kevin Mitaki</p>
+              <p>Test 15</p>
+            </div>
+            <p className={styles.time}>9:54 AM</p>
           </div>
           <div className={styles.msg_body}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-            odio dolor in, nesciunt officia dolores quaerat! Earum voluptatem
-            fugiat ut aliquid fugit, sint blanditiis quibusdam accusantium minus
-            et pariatur dignissimos?
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Similique odio dolor in, nesciunt officia dolores quaerat! Earum
+              voluptatem fugiat ut aliquid fugit, sint blanditiis quibusdam
+              accusantium minus et pariatur dignissimos?
+            </p>
+            <div className={styles.msg_time}>
+              <AiFillStar />
+              <p>10:30PM</p>
+            </div>
           </div>
         </div>
         <div className={styles.message}>
           <div className={styles.msg_header}>
             <img className={styles.profile_img} src="portitem1.jpeg" alt="" />
-            <p>Kevin Mitaki</p>
-            <p>Test 15</p>
+            <div className={styles.from_to}>
+              <p>Kevin Mitaki</p>
+              <p>Test 15</p>
+            </div>
+            <p className={styles.time}>9:54 AM</p>
           </div>
           <div className={styles.msg_body}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-            odio dolor in, nesciunt officia dolores quaerat! Earum voluptatem
-            fugiat ut aliquid fugit, sint blanditiis quibusdam accusantium minus
-            et pariatur dignissimos?
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Similique odio dolor in, nesciunt officia dolores quaerat! Earum
+              voluptatem fugiat ut aliquid fugit, sint blanditiis quibusdam
+              accusantium minus et pariatur dignissimos?
+            </p>
+            <div className={styles.msg_time}>
+              <AiFillStar />
+              <p>10:30PM</p>
+            </div>
           </div>
         </div>
         <div className={styles.message}>
           <div className={styles.msg_header}>
             <img className={styles.profile_img} src="portitem1.jpeg" alt="" />
-            <p>Kevin Mitaki</p>
-            <p>Test 15</p>
+            <div className={styles.from_to}>
+              <p>Kevin Mitaki</p>
+              <p>Test 15</p>
+            </div>
+            <p className={styles.time}>9:54 AM</p>
           </div>
           <div className={styles.msg_body}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-            odio dolor in, nesciunt officia dolores quaerat! Earum voluptatem
-            fugiat ut aliquid fugit, sint blanditiis quibusdam accusantium minus
-            et pariatur dignissimos?
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Similique odio dolor in, nesciunt officia dolores quaerat! Earum
+              voluptatem fugiat ut aliquid fugit, sint blanditiis quibusdam
+              accusantium minus et pariatur dignissimos?
+            </p>
+            <div className={styles.msg_time}>
+              <AiFillStar />
+              <p>10:30PM</p>
+            </div>
           </div>
         </div>
         <div className={styles.message}>
           <div className={styles.msg_header}>
             <img className={styles.profile_img} src="portitem1.jpeg" alt="" />
-            <p>Kevin Mitaki</p>
-            <p>Test 15</p>
+            <div className={styles.from_to}>
+              <p>Kevin Mitaki</p>
+              <p>Test 15</p>
+            </div>
+            <p className={styles.time}>9:54 AM</p>
           </div>
           <div className={styles.msg_body}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique
-            odio dolor in, nesciunt officia dolores quaerat! Earum voluptatem
-            fugiat ut aliquid fugit, sint blanditiis quibusdam accusantium minus
-            et pariatur dignissimos?
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Similique odio dolor in, nesciunt officia dolores quaerat! Earum
+              voluptatem fugiat ut aliquid fugit, sint blanditiis quibusdam
+              accusantium minus et pariatur dignissimos?
+            </p>
+            <div className={styles.msg_time}>
+              <AiFillStar />
+              <p>10:30PM</p>
+            </div>
           </div>
         </div>
       </div>
